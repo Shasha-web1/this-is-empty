0ad8bf7cf98358ddcfd79e7cc4fa8d723883b143
Shasha-web1 <sha.137.sha@gmail.com>
2025-11-14 15:22:35 +0000

    change

--- SQA-metrics-project/src/main/java/com/example/sqa/QualityMetrics.java
+++ SQA-metrics-project/src/main/java/com/example/sqa/QualityMetrics.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Objects;
 
 public final class QualityMetrics {
-    private QualityMetrics() {}
+    private QualityMetrics() {
+    
+    }
 
     public static double defectDensity(int defects, double kloc) {
         validateNonNegative(defects, "defects");
